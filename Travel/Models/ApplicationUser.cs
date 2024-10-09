a9dd89b0bc90f17f5fd3ad1808fc4730768e13e1
sara-ali5 <66339549+sara-ali5@users.noreply.github.com>
2024-10-09 04:14:16 +0300

    Add ApplicationUser.cs

---
+++ Travel/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Travel.Models
+{
+    public class ApplicationUser :IdentityUser
+    {
+        public string FullName { get; set; }
+
+
+        // Navigation property
+        public ICollection<Booking> Bookings { get; set; }
+    }
+}
