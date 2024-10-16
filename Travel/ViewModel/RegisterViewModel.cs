43b2521e29ef35ca3e8d9c838b4879c3b69cc98d
sara-ali5 <66339549+sara-ali5@users.noreply.github.com>
2024-10-16 22:16:41 +0300

    update

--- Travel/ViewModel/RegisterViewModel.cs
+++ Travel/ViewModel/RegisterViewModel.cs
@@ -8,13 +8,20 @@ namespace Travel.ViewModel
         [StringLength(100)]
         public string FullName { get; set; }
 
-        [Required]
-        [EmailAddress]
+       
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
         public string Email { get; set; }
 
-        [Required]
+        
+
         [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 8)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
         public string Password { get; set; }
+
+
 
         [Required]
         [DataType(DataType.Password)]
